a7ded38ff5d1601b716cacea300b78544dbfd39a
Artem Balan <a.s.balan96@gmail.com>
2024-01-20 19:03:13 +0200

    tests

--- src/test/java/com/example/sqch07ex1/MainTests.java
+++ src/test/java/com/example/sqch07ex1/MainTests.java
@@ -1,13 +1,26 @@
 package com.example.sqch07ex1;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class MainTests {
 
+	@Autowired
+	private MockMvc mockMvc;
+
 	@Test
-	void contextLoads() {
+	@DisplayName("Test /home page can be successfully called")
+	void testRequestAndContent() throws Exception{
+		mockMvc.perform(get("/home")).andExpect(status().isOk());
 	}
 
 }
